66241536e89630e7c1ccafeace3d6d73889d1fe1
arianna-cosimini <arianna.cosimini@gmail.com>
2024-04-29 19:47:52 +0200

    feat:bonus 2 eager-loading

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -8,13 +8,17 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         // $projects = Project::all();
-        $projects = Project::paginate(2);
+        // $projects = Project::paginate(2);
+
+        // per ricevere i progetti  tutte le tecnologie e i tipi collegati
+        $projects = Project::with(['types', 'technologies'])->paginate(2);
 
         return response()->json([
-            "success"=> true,
-            "results"=> $projects
+            "success" => true,
+            "results" => $projects
         ]);
     }
 }
