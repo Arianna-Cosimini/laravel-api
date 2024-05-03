372bf0166a749db422cc9f9f8a08b16e03834f76
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-05-03 14:12:03 +0200

    feat: contact mail

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LeadController;
 use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,3 +24,5 @@ Route::get('/projects', [PostController::class, 'index']);
 
 //rotta per la show singola
 Route::get('/projects/{slug}', [PostController::class, 'show']);
+
+Route::post('/newcontact', [LeadController::class, 'store']);
